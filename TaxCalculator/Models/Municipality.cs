e95957c520e7fdf33ebcb19be94eabdbdd410c55
Niclas <Niclas@Bengtsen.dk>
2024-08-25 13:59:45 +0200

    added models-folder and models

---
+++ TaxCalculator/Models/Municipality.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaxCalculator.Models
+{
+    public class Municipality
+    {
+        [Key]
+        public int MunicipalityCode { get; set; }
+        public string Name { get; set; }
+        public ICollection<TaxRecord> TaxRecords { get; set; }
+    }
+}
---
+++ TaxCalculator/Models/TaxRecord.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaxCalculator.Models
+{
+    public class TaxRecord
+    {
+        [Key]
+        public int Id { get; set; }
+        public required int MunicipalityId { get; set; }
+        public decimal TaxRate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
