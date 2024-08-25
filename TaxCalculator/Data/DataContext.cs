e690a0408fdde2c1e3d39313d5aa4d85af4e67fa
Niclas <Niclas@Bengtsen.dk>
2024-08-25 14:07:42 +0200

    setup in-memory database

---
+++ TaxCalculator/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaxCalculator.Models;
+
+namespace TaxCalculator.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Municipality> Municipalities { get; set; }
+        public DbSet<TaxRecord> TaxRecords { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
--- TaxCalculator/Program.cs
+++ TaxCalculator/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using TaxCalculator.Data;
+
 namespace TaxCalculator
 {
     public class Program
@@ -8,6 +11,9 @@ namespace TaxCalculator
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddDbContext<DataContext>(options =>
+            options.UseInMemoryDatabase(builder.Configuration.GetConnectionString("TaxRecordDb")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
